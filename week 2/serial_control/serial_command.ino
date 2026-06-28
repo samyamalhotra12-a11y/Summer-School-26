8280c6117e69d54c42e393ff086c17f3fdffb79f
VidishGupta-7 <vidishgupta67@gmail.com>
2026-06-28 21:49:43 +0530

    docs ; code uploaded

---
+++ week 2/serial_control/serial_command.ino
@@ -0,0 +1,53 @@
+const int ledPin = LED_BUILTIN;
+int blinkCount = 0; 
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("System Ready. Enter command:");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim(); 
+    if (cmd == "LED_ON") {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("OK: LED is ON");
+    } 
+    else if (cmd == "LED_OFF") {
+      digitalWrite(ledPin, LOW);
+      Serial.println("OK: LED is OFF");
+    } 
+    else if (cmd.startsWith("BLINK_")) {
+      
+      char xChar = cmd.charAt(6); 
+      
+     
+      if (xChar >= '1' && xChar <= '9') {
+        int x = xChar - '0'; 
+        for (int i = 0; i < x; i++) {
+          digitalWrite(ledPin, HIGH); delay(500);
+          digitalWrite(ledPin, LOW); delay(500);
+        }
+        blinkCount += x;
+        Serial.print("OK: Blinked "); Serial.print(x); Serial.println(" times");
+      } else {
+        Serial.println("ERROR: Invalid number (1-9)");
+      }
+    } 
+    else if (cmd == "STATUS") {
+      Serial.print("LED State: ");
+      Serial.println(digitalRead(ledPin) ? "ON" : "OFF");
+      Serial.print("Total Blinks Performed: ");
+      Serial.println(blinkCount);
+    } 
+    else if (cmd == "RESET") {
+      blinkCount = 0;
+      Serial.println("OK: Counter Reset");
+    } 
+    else {
+      Serial.println("ERROR: Unknown command");
+    }
+  }
+}
